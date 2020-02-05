08e3ab381b1de7e290b44bc759073e111ce336a2
yoshoku <yoshoku@outlook.com>
2020-02-05 23:14:01 +0900

    :sparkles: Add 32-bit hashing method

--- lib/mmh3.rb
+++ lib/mmh3.rb
@@ -1,6 +1,59 @@
-require "mmh3/version"
+# frozen_string_literal: true
+
+require 'mmh3/version'
 
 module Mmh3
-  class Error < StandardError; end
-  # Your code goes here...
+  module_function
+
+  def hash32(key_str, seed = 0)
+    key_str = key_str.to_s unless key_str.is_a?(String)
+    keyb = key_str.bytes
+    key_len = keyb.size
+    n_blocks = key_len / 4
+
+    h = seed
+    (0...n_blocks * 4).step(4) do |bstart|
+      k = keyb[bstart + 3] << 24 | keyb[bstart + 2] << 16 | keyb[bstart + 1] << 8 | keyb[bstart + 0]
+      h ^= scramble32(k)
+      h = rotl32(h, 13)
+      h = (h * 5 + 0xe6546b64) & 0xFFFFFFFF
+    end
+
+    tail_id = n_blocks * 4
+    tail_sz = key_len & 3
+
+    k = 0
+    k ^= keyb[tail_id + 2] << 16 if tail_sz >= 3
+    k ^= keyb[tail_id + 1] << 8 if tail_sz >= 2
+    k ^= keyb[tail_id + 0] if tail_sz >= 1
+    h ^= scramble32(k) if tail_sz.positive?
+
+    h = fmix32(h ^ key_len)
+
+    if (h & 0x80000000).zero?
+      h
+    else
+      -((h ^ 0xFFFFFFFF) + 1)
+    end
+  end
+
+  def rotl32(x, r)
+    (x << r | x >> (32 - r)) & 0xFFFFFFFF
+  end
+
+  def scramble32(k)
+    k = (k * 0xcc9e2d51) & 0xFFFFFFFF
+    k = rotl32(k, 15)
+    (k * 0x1b873593) & 0xFFFFFFFF
+  end
+
+  def fmix32(h)
+    h ^= h >> 16
+    h = (h * 0x85ebca6b) & 0xFFFFFFFF
+    h ^= h >> 13
+    h = (h * 0xc2b2ae35) & 0xFFFFFFFF
+    h ^ (h >> 16)
+  end
+
+  private_class_method :rotl32, :scramble32, :fmix32
 end
