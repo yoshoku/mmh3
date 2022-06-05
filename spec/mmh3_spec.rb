ea8aa9704fd2f00744ff1c58bcf6db9c60079651
yoshoku <yoshoku@outlook.com>
2022-06-05 09:54:41 +0900

    chore: disable Layout/LineLength cop

--- spec/mmh3_spec.rb
+++ spec/mmh3_spec.rb
@@ -6,16 +6,20 @@ RSpec.describe Mmh3 do
     it { expect(described_class.hash32('Hello, world', seed: 10)).to eq(-172601702) }
   end
 
+  # rubocop:disable Layout/LineLength
   describe '#hash128 (x64)' do
     it { expect(described_class.hash128('Hello, world', seed: 0)).to eq(158517598496188337575393694976300464500) }
     it { expect(described_class.hash128('Hello, world', seed: 80)).to eq(30039177286814921195667057753583847313) }
     it { expect(described_class.hash128('Hello, world. Hello, world. Hello, world.', seed: 10)).to eq(9261380712901568808277265119757985890) }
   end
+  # rubocop:enable Layout/LineLength
 
+  # rubocop:disable Layout/LineLength
   describe '#hash128 (x86)' do
     it { expect(described_class.hash128('Hello, world', seed: 0, x64arch: false)).to eq(253056019824187517714158156925852552360) }
     it { expect(described_class.hash128('Hello, world', seed: 7, x64arch: false)).to eq(52510795136989075550025607058488316817) }
     it { expect(described_class.hash128('Hello, world. Hello, world. Hello, world.', seed: 10, x64arch: false)).to eq(226892773628257154411244924604649453748) }
     it { expect(described_class.hash128('heelloo, world!', seed: 9, x64arch: false)).to eq(275876218145385640994298123654585548801) }
   end
+  # rubocop:enable Layout/LineLength
 end
