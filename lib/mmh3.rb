11778633631388a8f4272cdf40d96961475c7a6a
yoshoku <yoshoku@outlook.com>
2022-06-05 10:30:51 +0900

    chore: disable some layout cops

--- lib/mmh3.rb
+++ lib/mmh3.rb
@@ -31,16 +31,18 @@ module Mmh3
       k = block32(keyb, bstart, 0)
       h ^= scramble32(k)
       h = rotl32(h, 13)
-      h = (h * 5 + 0xe6546b64) & 0xFFFFFFFF
+      h = ((h * 5) + 0xe6546b64) & 0xFFFFFFFF
     end
 
     tail_id = n_blocks * 4
     tail_sz = key_len & 3
 
     k = 0
+    # rubocop:disable Layout/ExtraSpacing, Layout/SpaceAroundOperators
     k ^= keyb[tail_id + 2] << 16 if tail_sz >= 3
     k ^= keyb[tail_id + 1] <<  8 if tail_sz >= 2
     k ^= keyb[tail_id + 0]       if tail_sz >= 1
+    # rubocop:enable Layout/ExtraSpacing, Layout/SpaceAroundOperators
 
     h ^= scramble32(k) if tail_sz.positive?
 
@@ -94,7 +96,7 @@ module Mmh3
 
       h1 = rotl64(h1, 27)
       h1 = (h1 + h2) & 0xFFFFFFFFFFFFFFFF
-      h1 = (h1 * 5 + 0x52dce729) & 0xFFFFFFFFFFFFFFFF
+      h1 = ((h1 * 5) + 0x52dce729) & 0xFFFFFFFFFFFFFFFF
 
       k2 = (k2 * c2) & 0xFFFFFFFFFFFFFFFF
       k2 = rotl64(k2, 33)
@@ -103,13 +105,14 @@ module Mmh3
 
       h2 = rotl64(h2, 31)
       h2 = (h2 + h1) & 0xFFFFFFFFFFFFFFFF
-      h2 = (h2 * 5 + 0x38495ab5) & 0xFFFFFFFFFFFFFFFF
+      h2 = ((h2 * 5) + 0x38495ab5) & 0xFFFFFFFFFFFFFFFF
     end
 
     tail_id = n_blocks * 16
     tail_sz = key_len & 15
 
     k2 = 0
+    # rubocop:disable Layout/ExtraSpacing, Layout/SpaceAroundOperators
     k2 ^= keyb[tail_id + 14] << 48 if tail_sz >= 15
     k2 ^= keyb[tail_id + 13] << 40 if tail_sz >= 14
     k2 ^= keyb[tail_id + 12] << 32 if tail_sz >= 13
@@ -117,6 +120,7 @@ module Mmh3
     k2 ^= keyb[tail_id + 10] << 16 if tail_sz >= 11
     k2 ^= keyb[tail_id +  9] <<  8 if tail_sz >= 10
     k2 ^= keyb[tail_id +  8]       if tail_sz >=  9
+    # rubocop:enable Layout/ExtraSpacing, Layout/SpaceAroundOperators
 
     if tail_sz > 8
       k2 = (k2 * c2) & 0xFFFFFFFFFFFFFFFF
@@ -126,6 +130,7 @@ module Mmh3
     end
 
     k1 = 0
+    # rubocop:disable Layout/ExtraSpacing, Layout/SpaceAroundOperators
     k1 ^= keyb[tail_id + 7] << 56 if tail_sz >= 8
     k1 ^= keyb[tail_id + 6] << 48 if tail_sz >= 7
     k1 ^= keyb[tail_id + 5] << 40 if tail_sz >= 6
@@ -134,6 +139,7 @@ module Mmh3
     k1 ^= keyb[tail_id + 2] << 16 if tail_sz >= 3
     k1 ^= keyb[tail_id + 1] <<  8 if tail_sz >= 2
     k1 ^= keyb[tail_id]           if tail_sz >= 1
+    # rubocop:enable Layout/ExtraSpacing, Layout/SpaceAroundOperators
 
     if tail_sz > 0
       k1 = (k1 * c1) & 0xFFFFFFFFFFFFFFFF
@@ -154,7 +160,7 @@ module Mmh3
     h1 = (h1 + h2) & 0xFFFFFFFFFFFFFFFF
     h2 = (h1 + h2) & 0xFFFFFFFFFFFFFFFF
 
-    h2 << 64 | h1
+    (h2 << 64) | h1
   end
 
   def hash128_x86(key, seed = 0)
@@ -184,7 +190,7 @@ module Mmh3
 
       h1 = rotl32(h1, 19)
       h1 = (h1 + h2) & 0xFFFFFFFF
-      h1 = (h1 * 5 + 0x561ccd1b) & 0xFFFFFFFF
+      h1 = ((h1 * 5) + 0x561ccd1b) & 0xFFFFFFFF
 
       k2 = (k2 * c2) & 0xFFFFFFFF
       k2 = rotl32(k2, 16)
@@ -193,7 +199,7 @@ module Mmh3
 
       h2 = rotl32(h2, 17)
       h2 = (h2 + h3) & 0xFFFFFFFF
-      h2 = (h2 * 5 + 0x0bcaa747) & 0xFFFFFFFF
+      h2 = ((h2 * 5) + 0x0bcaa747) & 0xFFFFFFFF
 
       k3 = (k3 * c3) & 0xFFFFFFFF
       k3 = rotl32(k3, 17)
@@ -202,7 +208,7 @@ module Mmh3
 
       h3 = rotl32(h3, 15)
       h3 = (h3 + h4) & 0xFFFFFFFF
-      h3 = (h3 * 5 + 0x96cd1c35) & 0xFFFFFFFF
+      h3 = ((h3 * 5) + 0x96cd1c35) & 0xFFFFFFFF
 
       k4 = (k4 * c4) & 0xFFFFFFFF
       k4 = rotl32(k4, 18)
@@ -211,16 +217,18 @@ module Mmh3
 
       h4 = rotl32(h4, 13)
       h4 = (h4 + h1) & 0xFFFFFFFF
-      h4 = (h4 * 5 + 0x32ac3b17) & 0xFFFFFFFF
+      h4 = ((h4 * 5) + 0x32ac3b17) & 0xFFFFFFFF
     end
 
     tail_id = n_blocks * 16
     tail_sz = key_len & 15
 
     k4 = 0
+    # rubocop:disable Layout/ExtraSpacing, Layout/SpaceAroundOperators
     k4 ^= keyb[tail_id + 14] << 16 if tail_sz >= 15
     k4 ^= keyb[tail_id + 13] <<  8 if tail_sz >= 14
     k4 ^= keyb[tail_id + 12]       if tail_sz >= 13
+    # rubocop:enable Layout/ExtraSpacing, Layout/SpaceAroundOperators
 
     if tail_sz > 12
       k4 = (k4 * c4) & 0xFFFFFFFF
@@ -230,10 +238,12 @@ module Mmh3
     end
 
     k3 = 0
+    # rubocop:disable Layout/ExtraSpacing, Layout/SpaceAroundOperators
     k3 ^= keyb[tail_id + 11] << 24 if tail_sz >= 12
     k3 ^= keyb[tail_id + 10] << 16 if tail_sz >= 11
     k3 ^= keyb[tail_id +  9] <<  8 if tail_sz >= 10
     k3 ^= keyb[tail_id +  8]       if tail_sz >=  9
+    # rubocop:enable Layout/ExtraSpacing, Layout/SpaceAroundOperators
 
     if tail_sz > 8
       k3 = (k3 * c3) & 0xFFFFFFFF
@@ -243,10 +253,12 @@ module Mmh3
     end
 
     k2 = 0
+    # rubocop:disable Layout/ExtraSpacing, Layout/SpaceAroundOperators
     k2 ^= keyb[tail_id +  7] << 24 if tail_sz >=  8
     k2 ^= keyb[tail_id +  6] << 16 if tail_sz >=  7
     k2 ^= keyb[tail_id +  5] <<  8 if tail_sz >=  6
     k2 ^= keyb[tail_id +  4]       if tail_sz >=  5
+    # rubocop:enable Layout/ExtraSpacing, Layout/SpaceAroundOperators
 
     if tail_sz > 4
       k2 = (k2 * c2) & 0xFFFFFFFF
@@ -256,10 +268,12 @@ module Mmh3
     end
 
     k1 = 0
+    # rubocop:disable Layout/ExtraSpacing, Layout/SpaceAroundOperators
     k1 ^= keyb[tail_id +  3] << 24 if tail_sz >=  4
     k1 ^= keyb[tail_id +  2] << 16 if tail_sz >=  3
     k1 ^= keyb[tail_id +  1] <<  8 if tail_sz >=  2
     k1 ^= keyb[tail_id]            if tail_sz >=  1
+    # rubocop:enable Layout/ExtraSpacing, Layout/SpaceAroundOperators
 
     if tail_sz > 0
       k1 = (k1 * c1) & 0xFFFFFFFF
@@ -292,33 +306,37 @@ module Mmh3
     h3 = (h1 + h3) & 0xFFFFFFFF
     h4 = (h1 + h4) & 0xFFFFFFFF
 
-    h4 << 96 | h3 << 64 | h2 << 32 | h1
+    (h4 << 96) | (h3 << 64) | (h2 << 32) | h1
   end
 
   def block32(kb, bstart, offset)
-    kb[bstart + offset + 3] << 24 |
-    kb[bstart + offset + 2] << 16 |
-    kb[bstart + offset + 1] <<  8 |
-    kb[bstart + offset]
+    # rubocop:disable Layout/ExtraSpacing, Layout/SpaceAroundOperators, Layout/MultilineOperationIndentation
+    (kb[bstart + offset + 3] << 24) |
+    (kb[bstart + offset + 2] << 16) |
+    (kb[bstart + offset + 1] <<  8) |
+     kb[bstart + offset]
+    # rubocop:enable Layout/ExtraSpacing, Layout/SpaceAroundOperators, Layout/MultilineOperationIndentation
   end
 
   def block64(kb, bstart, offset)
-    kb[2 * bstart + (7 + offset)] << 56 |
-    kb[2 * bstart + (6 + offset)] << 48 |
-    kb[2 * bstart + (5 + offset)] << 40 |
-    kb[2 * bstart + (4 + offset)] << 32 |
-    kb[2 * bstart + (3 + offset)] << 24 |
-    kb[2 * bstart + (2 + offset)] << 16 |
-    kb[2 * bstart + (1 + offset)] <<  8 |
-    kb[2 * bstart + offset]
+    # rubocop:disable Layout/ExtraSpacing, Layout/SpaceAroundOperators, Layout/MultilineOperationIndentation
+    (kb[(2 * bstart) + (7 + offset)] << 56) |
+    (kb[(2 * bstart) + (6 + offset)] << 48) |
+    (kb[(2 * bstart) + (5 + offset)] << 40) |
+    (kb[(2 * bstart) + (4 + offset)] << 32) |
+    (kb[(2 * bstart) + (3 + offset)] << 24) |
+    (kb[(2 * bstart) + (2 + offset)] << 16) |
+    (kb[(2 * bstart) + (1 + offset)] <<  8) |
+     kb[(2 * bstart) + offset]
+    # rubocop:enable Layout/ExtraSpacing, Layout/SpaceAroundOperators, Layout/MultilineOperationIndentation
   end
 
   def rotl32(x, r)
-    (x << r | x >> (32 - r)) & 0xFFFFFFFF
+    ((x << r) | (x >> (32 - r))) & 0xFFFFFFFF
   end
 
   def rotl64(x, r)
-    (x << r | x >> (64 - r)) & 0xFFFFFFFFFFFFFFFF
+    ((x << r) | (x >> (64 - r))) & 0xFFFFFFFFFFFFFFFF
   end
 
   def scramble32(k)
